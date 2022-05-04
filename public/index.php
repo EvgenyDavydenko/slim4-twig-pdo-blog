1ad0a8cdbeec2bf9e74f7416eda253c522a2c3ae
EvgenyDavydenko <frontend82@mail.ru>
2022-05-04 15:56:14 +0300

    initial setup

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Instantiate app
+$app = AppFactory::create();
+
+// Add Error Handling Middleware
+$app->addErrorMiddleware(true, false, false);
+
+// Add route callbacks
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello Slim4-Twig-PDO-Blog!');
+    return $response;
+});
+
+$app->get('/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Run application
+$app->run();
